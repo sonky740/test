e9ee664ac729b6958dc65db1204ab3ddf571612e
Sonky <sonky740@gmail.com>
2022-10-10 21:53:09 +0900

    canvas scale

--- canvas/minute/07-interaction/Panel.js
+++ canvas/minute/07-interaction/Panel.js
@@ -1,25 +1,15 @@
 class Panel {
   constructor() {
-    this.scale = 0.01;
-    this.angle = 0;
+    this.scale = 0;
   }
 
   draw() {
-    context.fillStyle = 'rgba(255,0,0,0.8)';
-    // 변환 초기화;
-    context.resetTransform();
-    // context.setTransform(1,0,0,1,0,0);
-    context.translate(oX, oY);
-    context.scale(this.scale, this.scale);
-    context.rotate(canUtil.toRadian(this.angle));
-    context.translate(-oX, -oY);
+    context.fillStyle = 'rgba(255, 0, 0, .7)';
     context.fillRect(oX - 150, oY - 150, 300, 300);
-    context.resetTransform();
-  }
 
-  showContent() {
-    console.log('showContent 실행');
     context.fillStyle = '#fff';
-    context.fillText(selectedBox.index, oX, oY);
+    if (selectedBox) {
+      context.fillText(selectedBox.index, oX, oY);
+    }
   }
 }
