1bbf9bab5bfc53c7da4d7aec8512d2041f490c62
Sonky <sonky740@gmail.com>
2022-10-10 22:27:49 +0900

    1분코딩 캔버스 테스트

--- canvas/minute/07-interaction/Panel.js
+++ canvas/minute/07-interaction/Panel.js
@@ -1,15 +1,23 @@
 class Panel {
   constructor() {
     this.scale = 0;
+    this.angle = 0;
   }
 
   draw() {
     context.fillStyle = 'rgba(255, 0, 0, .7)';
+    context.resetTransform();
+    context.translate(oX, oY);
+    context.scale(this.scale, this.scale);
+    context.rotate(canUtil.toRadian(this.angle));
+    context.translate(-oX, -oY);
     context.fillRect(oX - 150, oY - 150, 300, 300);
+    context.resetTransform();
+  }
 
+  showContent() {
+    console.log('showContent 실행')
     context.fillStyle = '#fff';
-    if (selectedBox) {
-      context.fillText(selectedBox.index, oX, oY);
-    }
+    context.fillText(selectedBox.index, oX, oY);
   }
 }
