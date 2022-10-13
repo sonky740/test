73a38a6aa16cac80e723bddf7f0dc37e97687dec
Sonky <sonky740@gmail.com>
2022-10-14 00:42:30 +0900

    xhr

--- javascript/http_request/assets/scripts/app.js
+++ javascript/http_request/assets/scripts/app.js
@@ -1,12 +1,22 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
 
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
-xhr.send();
+
+xhr.responseType = 'json';
+
 xhr.onload = function () {
-  if (xhr.status != 200) {
-    console.log(`Error ${xhr.status}: ${xhr.statusText}`);
-  } else {
-    console.log(xhr.response);
+  const listOfPosts = xhr.response;
+
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector('h2').textContent = post.title.toUpperCase();
+    postEl.querySelector('p').textContent = post.body;
+    listElement.append(postEl);
   }
 };
+
 xhr.onerror = function () {};
+
+xhr.send();
